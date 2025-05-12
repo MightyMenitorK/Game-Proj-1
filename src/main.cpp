90238aa45c6f6b22186bc508759d8bfb7519754f
nkmar <khathu@olivierzoppi.com>
2025-05-12 13:01:41 +0400

    hello world

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+
+int main (int argc, char* argv[]) {
+    // This is a simple C++ program that prints "Hello, World!" to the console.
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
